1dd406a10bddb1f1b0821b214553ca5bc8feea39
dev-kdoctor <74884225+dev-kdoctor@users.noreply.github.com>
2024-06-28 17:57:49 +0900

    테스트

---
+++ db.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'my_user',
+	password: 'Kdoc0706^^',
+	database: 'Kdoc0706^^',
+	port: 3308, // 기본 포트 번호
+});
+
+connection.connect((err) => {
+	if (err) {
+		console.error('Error connecting: ' + err.stack);
+		return;
+	}
+	console.log('Connected as id ' + connection.threadId);
+});
+
+module.exports = connection;
